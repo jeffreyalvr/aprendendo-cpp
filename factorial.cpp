40b7e81f202f9eeb9717930858ef93ffa4e0f2fc
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-23 23:36:02 -0300

    Adiciona calculadora fatorial

---
+++ factorial.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/* Calculadora de fatorial */
+
+void getFactorial(int number);
+
+int main() {
+  int number{};
+
+  do {
+    std::cout << "Type any number higher than 0: ";
+    std::cin >> number;
+  } while (number <= 0);
+
+  getFactorial(number);
+
+  return 0;
+}
+
+void getFactorial(int number) {
+  std::cout << number << "! = ";
+  int result = 1;
+
+  for (int i = number; i >= 1; i--) {
+    i == 1 ? std::cout << i : std::cout << i << " x ";
+    result *= i;
+  }
+  
+  std::cout << " = " << result << std::endl;
+}
